e4d771e8923c4f28e2801ea939dd2842cd892373
maketheswitch <azakani91@gmail.com>
2022-02-06 22:15:16 +0100

    Update connectBD.php

--- modele/connectBD.php
+++ modele/connectBD.php
@@ -1,5 +1,5 @@
 <?php
-	$hostname = "localhost";	
+	/*$hostname = "localhost";	
 	$base= "BDD_lagaragerie";
 	$loginBD= "root";	
 	$passBD="";
@@ -29,5 +29,14 @@
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	  }*/
+	
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 
 ?>

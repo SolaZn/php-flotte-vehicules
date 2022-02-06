686cdcadaada3142c8b17e86571c8dd17c7c350b
maketheswitch <azakani91@gmail.com>
2022-02-06 21:53:34 +0100

    Update connectBD.php

--- modele/connectBD.php
+++ modele/connectBD.php
@@ -10,13 +10,11 @@
 	$cleardb_password = $cleardb_url["pass"];
 	$cleardb_db = substr($cleardb_url["path"],1);
 	
-	echo(getenv("CLEARDB_DATABASE_URL"));
-
 	$active_group = 'default';
 	$query_builder = TRUE;
 
-	/*try {
-		$dsn = "mysql:server=$cleardb_server;dbname=$cleardb_db";
+	try {
+		$dsn = "mysql:server=". $cleardb_server.";dbname=". $cleardb_db;
 		$pdo = new PDO ($dsn, $cleardb_username, $cleardb_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
@@ -24,11 +22,12 @@
 		echo $dsn;
 		echo utf8_encode("Echec de connexion : " . $e->getMessage());
 		die();
-	}*/
+	}
+	/*
 	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
-	  }
+	  }*/
 
 ?>

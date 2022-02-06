4ea728267c3972e7eb9e9ae8afe2ab2565905df1
maketheswitch <azakani91@gmail.com>
2022-02-06 21:48:04 +0100

    Update connectBD.php

--- modele/connectBD.php
+++ modele/connectBD.php
@@ -15,7 +15,7 @@
 	$active_group = 'default';
 	$query_builder = TRUE;
 
-	try {
+	/*try {
 		$dsn = "mysql:server=$cleardb_server;dbname=$cleardb_db";
 		$pdo = new PDO ($dsn, $cleardb_username, $cleardb_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -24,5 +24,11 @@
 		echo $dsn;
 		echo utf8_encode("Echec de connexion : " . $e->getMessage());
 		die();
-	}
+	}*/
+	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	  }
+
 ?>

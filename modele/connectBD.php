4d1167bac6f832f433c39ae49b3f6c456f4af02e
maketheswitch <azakani91@gmail.com>
2022-02-06 21:55:59 +0100

    Update connectBD.php

--- modele/connectBD.php
+++ modele/connectBD.php
@@ -15,6 +15,7 @@
 
 	try {
 		$dsn = "mysql:server=". $cleardb_server.";dbname=". $cleardb_db;
+		echo $dsn;
 		$pdo = new PDO ($dsn, $cleardb_username, $cleardb_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}

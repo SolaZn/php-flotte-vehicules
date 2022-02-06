2194dc51b8026c35a2cbef51c9136122953b92c8
maketheswitch <azakani91@gmail.com>
2022-02-06 22:22:44 +0100

    Update connectBD.php

--- modele/connectBD.php
+++ modele/connectBD.php
@@ -30,7 +30,7 @@
 		die("Connection failed: " . $conn->connect_error);
 	  }*/
 	
-	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+/*	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
 $username = $url["user"];
@@ -38,6 +38,38 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-$pdo = new PDO ("mysql:server=".$server.";dbname=".$db, $username, $password);
+$pdo = new PDO ("mysql:server=".$server.";dbname=".$db, $username, $password);*/
+$dbstr = getenv('CLEARDB_DATABASE_URL');
+
+$dbstr = substr("$dbstr", 8);
+$dbstrarruser = explode(":", $dbstr);
+
+//Please don't look at these names. Yes I know that this is a little bit trash :D
+$dbstrarrhost = explode("@", $dbstrarruser[1]);
+$dbstrarrrecon = explode("?", $dbstrarrhost[1]);
+$dbstrarrport = explode("/", $dbstrarrrecon[0]);
+
+$dbpassword = $dbstrarrhost[0];
+$dbhost = $dbstrarrport[0];
+$dbport = $dbstrarrport[0];
+$dbuser = $dbstrarruser[0];
+$dbname = $dbstrarrport[1];
+
+unset($dbstrarrrecon);
+unset($dbstrarrport);
+unset($dbstrarruser);
+unset($dbstrarrhost);
+
+unset($dbstr);
+/*  //Uncomment this for debug reasons
+echo $dbname . " - name<br>";
+echo $dbhost . " - host<br>";
+echo $dbport . " - port<br>";
+echo $dbuser . " - user<br>";
+echo $dbpassword . " - passwd<br>";
+*/
+$dbanfang = 'mysql:host=' . $dbhost . ';dbname=' . $dbname;
+$db = new PDO($dbanfang, $dbuser, $dbpassword);
+//You can only use this with the standard port!
 
 ?>
